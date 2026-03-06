f76a4210e7850b123c1e58fdf1a8d4eed5c7856e
Emre Çetin <emrctnn11@gmail.com>
2026-03-06 23:02:59 +0300

    feat: type declerative fixes

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -8,12 +8,11 @@ import { provideThemeLeptonX } from '@abp/ng.theme.lepton-x';
 import { provideSideMenuLayout } from '@abp/ng.theme.lepton-x/layouts';
 
 bootstrapApplication(
-  withValidationBluePrint({
-    wrongPassword: 'Please choose 1q2w3E*'
-  }),AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers, 
+AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers, 
 provideAbpThemeShared(),
 provideThemeLeptonX(),
-provideSideMenuLayout()]}).catch(err => console.error(err))provideAbpThemeShared(
+provideSideMenuLayout()]}).catch(err => console.error(err));
+provideAbpThemeShared(
   withValidationBluePrint({
     wrongPassword: 'Please choose 1q2w3E*'
   }),);
